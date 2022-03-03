b8c1b14255c4aa29241dee4da452cbc7b286cb08
fsojye <josfyorobe@gmail.com>
2022-03-03 14:14:55 +0800

    update package dependencies

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -1,5 +1,19 @@
+
+import serverless_wsgi
+
 from apis.controllers import DataCollectorController
+from app import app  # Replace with your actual application
+
+# If you need to send additional content types as text, add then directly
+# to the whitelist:
+#
+# serverless_wsgi.TEXT_MIME_TYPES.append("application/custom+json")
+
+
+def handler(event, context):
+    return serverless_wsgi.handle_request(app.app, event, context)
 
 
 def data_collector_handler(event, context):
-    DataCollectorController.handle()
+    with app.app_context():
+        DataCollectorController.handle()
